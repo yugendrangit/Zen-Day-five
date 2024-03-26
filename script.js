837428ca426d77a4bfe5598055e8bb34ed48bef8
yugendrangit <yugeofficial.sr30@gmail.com>
2024-03-27 00:17:19 +0530

    completed

---
+++ script.js
@@ -0,0 +1,49 @@
+const obj = {
+    "name": "yugendran",
+    "DOB": "30 April 2002",
+    "contact": "1212547455",
+    "role": "developer",
+    "email": "abc@gmail.com",
+    "address": {
+      "city": "chennai",
+      "state": "TN",
+      "country": "IND",
+    },
+    "languages": {
+      "tamil": "native",
+      "english": "intermidiate",
+    },
+    "skills": ["observation", "decision making"],
+  };
+  
+  // From the above JSON, iterate over all for loops (for, for in, for of, forEach)
+  
+  // for loop :
+//   const data = Object.entries(obj);
+//   for (var i = 0; i < data.length; i++) {
+//     let [key, value] = data[i];
+//     console.log(value);
+//   }
+  
+  // for in :
+  
+//   for (var key in obj) {
+//     console.log(obj[key]);
+//   }
+  
+  // for of :
+  
+//   const data = Object.entries(obj);
+//   console.log(data);
+//   for (var [key, value] of data) {
+//     console.log(value);
+//   }
+  
+  // forEach :
+  
+//   const data = Object.entries(obj);
+//   console.log(data);
+//   data.forEach(([key, value]) => {
+//     console.log(value);
+//   });
+  
